86b233e998908f2b0af329e1f3def62f7e7b8d2c
catcosmo <astrangethought@hotmail.com>
2017-04-12 16:10:34 +0200

    le rest

--- src/Playa.java
+++ src/Playa.java
@@ -1,4 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import lenz.htw.bogapr.net.NetworkClient;
 
 public class Playa {
-
+    public static void main(String[] args) {
+        try {
+            NetworkClient networkClient = new NetworkClient(null, "CLIENT 0", ImageIO.read(new File("meinLogo.png")));
+            
+            //networkClient.getExpectedNetworkLatencyInMilliseconds();
+            //networkClient.getTimeLimitInSeconds();
+            //networkClient.getMyPlayerNumber();
+            
+            for (;;) {
+                Move receiveMove;
+                while ((receiveMove = networkClient.receiveMove()) != null) {
+                    //Zug in meine Brettrepräsentation einarbeiten
+                }
+                //berechne tollen Zug
+                networkClient.sendMove(new Move(abc));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("", e);
+        }
+    }
 }
