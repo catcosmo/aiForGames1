671a8b6641416e9d7e7d0858c64a5675fdc5cd1b
Lucas <s0539248@htw-berlin.de>
2017-04-12 21:18:14 +0200

    fixed setup

--- src/Playa.java
+++ src/Playa.java
@@ -3,27 +3,28 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import lenz.htw.bogapr.Move;
 import lenz.htw.bogapr.net.NetworkClient;
 
 public class Playa {
-    public static void main(String[] args) {
-        try {
-            NetworkClient networkClient = new NetworkClient(null, "CLIENT 0", ImageIO.read(new File("meinLogo.png")));
-            
-            //networkClient.getExpectedNetworkLatencyInMilliseconds();
-            //networkClient.getTimeLimitInSeconds();
-            //networkClient.getMyPlayerNumber();
-            
-            for (;;) {
-                Move receiveMove;
-                while ((receiveMove = networkClient.receiveMove()) != null) {
-                    //Zug in meine Brettrepräsentation einarbeiten
-                }
-                //berechne tollen Zug
-                networkClient.sendMove(new Move(abc));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("", e);
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			NetworkClient networkClient = new NetworkClient(null, "CLIENT 0", ImageIO.read(new File("meinLogo.png")));
+
+			//networkClient.getExpectedNetworkLatencyInMilliseconds();
+			//networkClient.getTimeLimitInSeconds();
+			//networkClient.getMyPlayerNumber();
+
+			for (;;) {
+				Move receiveMove;
+				while ((receiveMove = networkClient.receiveMove()) != null) {
+					//Zug in meine Brettrepräsentation einarbeiten
+				}
+				//berechne tollen Zug
+				//networkClient.sendMove(new Move(abc));
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("", e);
+		}
+	}
 }
